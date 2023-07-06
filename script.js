084a68bf1fb0f92a259f5e8de81d280a91ca4557
sbaggar <138671954+sbaggar@users.noreply.github.com>
2023-07-06 19:15:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     window.addEventListener('scroll', checkSlide);
 });
+
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
